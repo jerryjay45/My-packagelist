62f8989d8a179cd53db0e9fe1f53e90eeebb4b8d
Shaq <jerryjay8000@gmail.com>
2026-06-19 11:01:39 -0500

    2nd commit

--- repo-install.sh
+++ repo-install.sh
@@ -1,60 +1,56 @@
 #!/bin/bash
 set -e
 
-OFFICIAL_PACKAGES=(
-    firefox
-    git
-)
+REPO_URL="https://github.com/jerryjay45/My-packagelist.git"
+WORKDIR="/tmp/My-packagelist"
 
-CACHYOS_PACKAGES=(
-    linux-cachyos
-)
-
-CHAOTIC_PACKAGES=(
-    proton-ge-custom-bin
-    yay
-)
-
-AUR_PACKAGES=(
-    visual-studio-code-bin
-)
-
-echo "Updating system..."
 sudo pacman -Syu --noconfirm
+sudo pacman -S --needed --noconfirm git curl base-devel
 
-echo "Installing prerequisites..."
-sudo pacman -S --needed --noconfirm curl git base-devel
+# Clone package lists
+rm -rf "$WORKDIR"
+git clone "$REPO_URL" "$WORKDIR"
 
+####################################
 # Install CachyOS repo
-if ! pacman -Sl cachyos >/dev/null 2>&1; then
-    curl -fsSL https://mirror.cachyos.org/cachyos-repo.tar.xz | tar -xJ
-    cd cachyos-repo
-    chmod +x cachyos-repo.sh
-    sudo ./cachyos-repo.sh
-    cd ..
-fi
+####################################
+curl -fsSL https://mirror.cachyos.org/cachyos-repo.tar.xz | tar -xJ
+cd cachyos-repo
+chmod +x cachyos-repo.sh
+sudo ./cachyos-repo.sh
+cd ..
 
+####################################
 # Install Chaotic-AUR repo
-if ! pacman -Sl chaotic-aur >/dev/null 2>&1; then
-    sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
-    sudo pacman-key --lsign-key 3056513887B78AEB
+####################################
+sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+sudo pacman-key --lsign-key 3056513887B78AEB
 
-    sudo pacman -U --noconfirm \
-        https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst \
-        https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst
+sudo pacman -U --noconfirm \
+https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst \
+https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst
 
-    echo '
+if ! grep -q "\[chaotic-aur\]" /etc/pacman.conf; then
+sudo tee -a /etc/pacman.conf <<EOF
+
 [chaotic-aur]
-Include = /etc/pacman.d/chaotic-mirrorlist' | sudo tee -a /etc/pacman.conf
+Include = /etc/pacman.d/chaotic-mirrorlist
+EOF
 fi
 
-sudo pacman -Sy
+sudo pacman -Syy
 
-# Install packages from repos
-sudo pacman -S --needed --noconfirm \
-    "${OFFICIAL_PACKAGES[@]}" \
-    "${CACHYOS_PACKAGES[@]}" \
-    "${CHAOTIC_PACKAGES[@]}"
+####################################
+# Install yay from Chaotic-AUR
+####################################
+sudo pacman -S --needed --noconfirm yay
 
-# Install AUR packages
-yay -S --needed --noconfirm "${AUR_PACKAGES[@]}"
+####################################
+# Install packages
+####################################
+cd "$WORKDIR"
+
+sudo pacman -S --needed $(< package-pacman.txt)
+yay -S --needed $(< aur-packages.txt)
+
+echo "Done."
